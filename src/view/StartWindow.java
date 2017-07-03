8de1f6f06959f0e774a803a81b3d0c9ea9068d37
didulidu <vlada996@gmail.com>
2017-07-03 17:25:19 +0200

    StartWindow doradjena tabela. Izrada jos u toku.

--- src/view/StartWindow.java
+++ src/view/StartWindow.java
@@ -15,96 +15,88 @@ import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
 import model.Aplikacija;
-import model.Main;
 import model.Obilazak;
 
-public class StartWindow extends JFrame
-        implements ItemListener {
+public class StartWindow extends JFrame implements ItemListener {
 
-    private JLabel display;
-    private JComboBox<String> box;
-    private String[] cities;
-    private Object[][] podaci;
-    private JTable tabela;
-    private String[] ime={"Obilasci: "};
-    private DefaultTableModel model;
-    
+	private JLabel display;
+	private JComboBox<String> box;
+	private String[] cities;
+	private Object[][] podaci = new Object[][]{};
+	private JTable tabela;
+	private String[] ime = { "<html><font color='red'>Obilasci: </font></html>"};
+	private DefaultTableModel model;
 
-    private void fillComboBox(){
-        box = new JComboBox<>();
-        box.addItem("");
-    	for (String  g : Aplikacija.gradovi.keySet())
-    		box.addItem(g);
-    }
-    
-    private void fillTable(String city){
-        model.setRowCount(0);
-    	for (Obilazak o : Aplikacija.sviObilasci.values()){
-    		if (o.getGrad().getNaziv().compareTo(city)==0){
-    	        model.addRow(new Object[]{o.getNaziv()});
-    	        System.out.println(o.getIdOb());
-    			System.out.println("Dodao  u tabelu");
-    		}
-    	}
-    	
-    	
-    	}
-    
-    public StartWindow() {
+	private void fillComboBox() {
+		box = new JComboBox<>();
+		box.addItem("");
+		for (String g : Aplikacija.gradovi.keySet())
+			box.addItem(g);
+	}
 
-    	fillComboBox();
-        initUI();
+	private void fillTable(String city) {
+		model.setRowCount(0);
+		
+		for (Obilazak o : Aplikacija.sviObilasci.values()) {
+			if (o.getGrad().getNaziv().compareTo(city) == 0) {
+				model.addRow(new Object[]{ o.getNaziv() });
+			}
+		}
+	}
 
-    }
+	public StartWindow() {
+		
+		fillComboBox();
+		initUI();
 
-    private void initUI() {
-    	setLayout(null);
+	}
 
-        display = new JLabel("Grad: ");
-        JLabel naslov = new JLabel(new ImageIcon("./src/slike/naslov.png"));
-        naslov.setBounds(140, 10, 677,98);
-        add(naslov);
-        getContentPane().setBackground(new Color(0,153,76));
-    	setSize(1000,800);
-    	display.setBounds(40, 200, 150,25);
-    	display.setFont(new Font("fontic",Font.BOLD, 20 ));
-    	box.setBounds(100,200,100,25);
-    	add(display);
-    	add(box);
-        box.addItemListener(this);        
-        setTitle("DinoMite");
-        ImageIcon image = new ImageIcon("./src/slike/naslov.png");
-        setIconImage(image.getImage());
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);     
+	private void initUI() {
+		setLayout(null);
 
-        setResizable(false);
-        tabela = new JTable(new DefaultTableModel(new Object[][]{}, ime));
-    	tabela.setRowHeight(30);
+		display = new JLabel("Grad: ");
+		JLabel naslov = new JLabel(new ImageIcon("./src/slike/naslov.png"));
+		naslov.setBounds(140, 10, 677, 98);
+		add(naslov);
+		getContentPane().setBackground(new Color(0, 153, 76));
+		setSize(1000, 800);
+		display.setBounds(40, 200, 150, 25);
+		display.setFont(new Font("fontic", Font.BOLD, 20));
+		box.setBounds(100, 200, 100, 25);
+		add(display);
+		add(box);
+		box.addItemListener(this);
+		setTitle("DinoMite");
+		ImageIcon image = new ImageIcon("./src/slike/dinamit.png");
+		setIconImage(image.getImage());
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		
+		setResizable(false);
+		
+		model = new DefaultTableModel(podaci, ime) {
+			@Override
+			public boolean isCellEditable(int row, int column) {
+				return false;
+			}
+		};
+		tabela = new JTable(model);
+		tabela.setFont(new Font("f", Font.BOLD, 30));
+		tabela.getTableHeader().setFont(new Font("SansSerif", Font.BOLD, 40));
+		
+		tabela.setRowHeight(50);
+		JScrollPane galerija = new JScrollPane(tabela);
+		galerija.setBounds(40, 300, 500, 400);
+		add(galerija);
 
-        model = (DefaultTableModel) tabela.getModel();
-        JScrollPane galerija = new JScrollPane(tabela);
-        galerija.setBounds(40,300,500,400);
-        add(galerija);
-        
+	}
 
-        
-    }
+	@Override
+	public void itemStateChanged(ItemEvent e) {
+		// Za svaki grad moramo prikazati ture
+		if (e.getStateChange() == ItemEvent.SELECTED) {
+			fillTable(e.getItem().toString());
+		}
+	}
 
-    @Override
-    public void itemStateChanged(ItemEvent e) {
-// Za svaki grad moramo prikazati ture
-        if (e.getStateChange() == ItemEvent.SELECTED) {
-            fillTable(e.getItem().toString());
-        }
-    }
-
-//    public static void main(String[] args) {
-//
-//        EventQueue.invokeLater(() -> {
-//            StartWindow ex = new StartWindow();
-//            ex.setVisible(true);
-//        });
-//    }
-//    
 }
