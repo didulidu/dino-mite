e8c9bb54dc30fc96af547b1adada7128b09ba2f5
didulidu <vlada996@gmail.com>
2017-07-03 19:38:32 +0200

    StartWintow (sa logIn-om) i RegistrationWindow odradjeni skoro.
    sledi jos jedan pull cim se vratim iz toaleta :)

--- src/view/RegistrationWindow.java
+++ src/view/RegistrationWindow.java
@@ -2,12 +2,14 @@ package view;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
 import model.Aplikacija;
@@ -41,42 +43,50 @@ public class RegistrationWindow extends JFrame{
 	
 	private void initUI(){
 		setLayout(null);
-		JButton ok = new JButton("Ok");
-		ok.setBounds(200-40, 300-50 , 80, 25);
-		JButton cancel = new JButton("Cancel");
-		cancel.setBounds(250, 250, 80, 25);
+		JButton ok = new JButton("<html><font color='green'>OK </font></html>");
+		ok.setBounds(50, 350 , 150, 30);
+		JButton cancel = new JButton("<html><font color='red'>Cancel</font></html>");
+		cancel.setBounds(250, 350, 150, 30);
 		
 		JLabel name = new JLabel("Name: ");
 		JLabel lName = new JLabel("Last name: ");
 		JLabel jmbg = new JLabel("JMBG: ");
 		JLabel street = new JLabel("Adress: ");
-		name.setBounds(100, 50 , 80, 25);
-		lName.setBounds(100, 75 , 80, 25);
-		jmbg.setBounds(100, 100 , 80, 25);
-		street.setBounds(100, 125 , 80, 25);
+		name.setFont(new Font("ss", Font.LAYOUT_LEFT_TO_RIGHT, 20));
+		lName.setFont(name.getFont());
+		jmbg.setFont(name.getFont());
+		street.setFont(name.getFont());
 		
-		JTextField nameT = new JTextField("*name*");
-		JTextField lNameT = new JTextField("lastname*");
-		JTextField jmbgT= new JTextField("*jmbg*");
-		JTextField streetT = new JTextField("*adress*");
-		nameT.setBounds(200, 50 , 80, 25);
-		lNameT.setBounds(200, 75 , 80, 25);
-		jmbgT.setBounds(200, 100 , 80, 25);
-		streetT.setBounds(200, 125 , 80, 25);
+		name.setBounds(50, 20 , 150, 25);
+		lName.setBounds(50, 70 , 150, 25);
+		jmbg.setBounds(50, 120 , 150, 25);
+		street.setBounds(50, 180 , 150, 25);
+		
+		JTextField nameT = new JTextField("");
+		JTextField lNameT = new JTextField("");
+		JTextField jmbgT= new JTextField("");
+		JTextField streetT = new JTextField("");
+		nameT.setBounds(150, 20 , 200, 30);
+		lNameT.setBounds(150, 70 , 200, 30);
+		jmbgT.setBounds(150, 120 , 200, 30);
+		streetT.setBounds(150, 180 , 200, 30);
 	
 		JLabel user = new JLabel("Username: ");
 		JLabel pass = new JLabel("Password: ");
-		user.setBounds(100, 150 , 80, 25);
-		pass.setBounds(100, 175 , 80, 25);
+		user.setBounds(50, 230 , 150, 30);
+		pass.setBounds(50, 280 , 150, 30);
+		user.setFont(name.getFont());
+		pass.setFont(name.getFont());
+		
 		JTextField userT= new JTextField();
-		userT.setBounds(200,150, 100,25);
-		JTextField passT= new JTextField();
-		passT.setBounds(200,175, 100,25);
+		userT.setBounds(150,230, 200,30);
+		JPasswordField passT= new JPasswordField();
+		passT.setBounds(150,280, 200,30);
 		
 		JLabel errT= new JLabel("<html><font color='red'>Error: Username taken.Please try again! </font></html>");
 		errT.setBounds(150,200, 150,60);
 		errT.setVisible(false);
-		setSize(new Dimension(400,400));
+		setSize(new Dimension(450,450));
 		setResizable(false);
 		add(cancel);
 		add(ok);
@@ -93,12 +103,13 @@ public class RegistrationWindow extends JFrame{
 		add(lNameT);
 		add(streetT);
 		add(jmbgT);
-		
+		getContentPane().setBackground(new Color(40, 200, 76));
+
 		setVisible(true);
 		setTitle("Log in to DinoMite!");
 		ImageIcon ikonica = new ImageIcon("ikonice/mite.png");
 		setIconImage(ikonica.getImage());
-		
+		setLocationRelativeTo(null);
 		// na klik cancel dugmeta :
 		cancel.addActionListener( (ActionEvent event) -> {
             dispose();
--- src/view/StartWindow.java
+++ src/view/StartWindow.java
@@ -1,32 +1,49 @@
 package view;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 
 import javax.swing.ImageIcon;
-
+import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPasswordField;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
 
 import model.Aplikacija;
+import model.Korisnik;
 import model.Obilazak;
 
 public class StartWindow extends JFrame implements ItemListener {
-
+	// Atributi
 	private JLabel display;
 	private JComboBox<String> box;
 	private String[] cities;
-	private Object[][] podaci = new Object[][]{};
+	private Object[][] podaci = new Object[][] {};
 	private JTable tabela;
-	private String[] ime = { "<html><font color='red'>Obilasci: </font></html>"};
+	private String[] ime = { "<html><font color='red'>Obilasci: </font></html>" };
 	private DefaultTableModel model;
+	private String username;
+	private String password;
 
+	// Proverava ispravnost unetih podataka pri logovanju
+	private Korisnik checkLogIn() {
+		for (Korisnik k : Aplikacija.korisnici.values()) {
+			if (k.getKorisnickoIme().compareTo(username) == 0 && k.getLozinka().compareTo(password) == 0)
+				return k;
+		}
+		return null;
+	}
+
+	// Popunjavanje Combo Boxa Gradovima koji postoje
 	private void fillComboBox() {
 		box = new JComboBox<>();
 		box.addItem("");
@@ -34,23 +51,24 @@ public class StartWindow extends JFrame implements ItemListener {
 			box.addItem(g);
 	}
 
+	// Popunjavanje Tabele Obilascima selektovanog grada
 	private void fillTable(String city) {
 		model.setRowCount(0);
-		
+
 		for (Obilazak o : Aplikacija.sviObilasci.values()) {
 			if (o.getGrad().getNaziv().compareTo(city) == 0) {
-				model.addRow(new Object[]{ o.getNaziv() });
+				model.addRow(new Object[] { o.getNaziv() });
 			}
 		}
 	}
 
+	// Konstruktor
 	public StartWindow() {
-		
 		fillComboBox();
 		initUI();
-
 	}
 
+	// Prozorche
 	private void initUI() {
 		setLayout(null);
 
@@ -62,19 +80,17 @@ public class StartWindow extends JFrame implements ItemListener {
 		setSize(1000, 800);
 		display.setBounds(40, 200, 150, 25);
 		display.setFont(new Font("fontic", Font.BOLD, 20));
-		box.setBounds(100, 200, 100, 25);  
+		box.setBounds(100, 200, 100, 25);
 		add(display);
 		add(box);
-		System.out.println("print samo da bih promenio fajl da bih mogao opet da pokusam da pushujem");
 		box.addItemListener(this);
 		setTitle("DinoMite");
 		ImageIcon image = new ImageIcon("./src/slike/dinamit.png");
 		setIconImage(image.getImage());
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
-		
 		setResizable(false);
-		
+
 		model = new DefaultTableModel(podaci, ime) {
 			@Override
 			public boolean isCellEditable(int row, int column) {
@@ -84,12 +100,71 @@ public class StartWindow extends JFrame implements ItemListener {
 		tabela = new JTable(model);
 		tabela.setFont(new Font("f", Font.BOLD, 30));
 		tabela.getTableHeader().setFont(new Font("SansSerif", Font.BOLD, 40));
-		
+
 		tabela.setRowHeight(50);
 		JScrollPane galerija = new JScrollPane(tabela);
 		galerija.setBounds(40, 300, 500, 400);
 		add(galerija);
+		// logIn deo
+		JButton ok = new JButton("<html><font color='green'>OK </font></html>");
+		ok.setBounds(730, 410, 100, 30);
+		JButton cancel = new JButton("<html><font color='red'>Cancel </font></html>");
+		cancel.setBounds(850, 410, 100, 30);
+		JLabel user = new JLabel("*Username: ");
+		JLabel pass = new JLabel("*Password: ");
+		user.setFont(new Font("f", Font.BOLD, 24));
+		pass.setFont(user.getFont());
+		user.setBounds(600, 300, 200, 40);
+		pass.setBounds(600, 350, 200, 40);
+		JTextField userT = new JTextField();
+		userT.setBounds(750, 310, 200, 25);
+		JPasswordField passT = new JPasswordField();
+		passT.setBounds(750, 360, 200, 25);
 
+		JLabel errT = new JLabel(
+				"<html><font color='red'>*Incorrect username and/or passwort.Please try again! </font></html>");
+		errT.setFont(new Font("error", Font.CENTER_BASELINE, 20));
+		errT.setBounds(600, 250, 350, 50);
+		errT.setVisible(false);
+		add(cancel);
+		add(ok);
+		add(user);
+		add(pass);
+		add(userT);
+		add(passT);
+		add(errT);
+		// na klik cancel dugmeta :
+		cancel.addActionListener((ActionEvent event) -> {
+			dispose();
+		});
+
+		// na klik ok dugmeta:
+		ok.addActionListener((ActionEvent event) -> {
+			username = userT.getText();
+			password = passT.getText();
+			System.out.println(password);
+			Korisnik k = checkLogIn();
+			if (k == null) {
+				errT.setVisible(true);
+				userT.setText("");
+				passT.setText("");
+			} else {
+				errT.setVisible(false);
+				dispose();
+			}
+		});
+		
+		//registracija
+		JButton regB = new JButton("<html><font color='blue'>Register </font></html>" );
+		JLabel regL = new JLabel("<html><font color='white'>Don't have an account? Register for FREE </font></html>");
+		
+		regL.setBounds(600, 440, 300, 50);
+		regB.setBounds(700, 480, 100, 30);
+		add(regL);
+		add(regB);
+		regB.addActionListener((ActionEvent event) -> {
+			RegistrationWindow regWin = new RegistrationWindow();
+		});
 	}
 
 	@Override
